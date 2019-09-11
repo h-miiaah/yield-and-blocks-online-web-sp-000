e45b1fc3df4c09b3c43d609369315e990211c303
Haim D <haim11293@aol.com>
2019-09-11 13:29:42 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,8 +8,7 @@ def hello_t(array)
 end
 
 # call your method here!
-array = ["Tim", "Tom", "Jim"]
-hello_t(array) do |name|
+hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.starts_with?("T")
     puts "Hi, #{name}"
   end
